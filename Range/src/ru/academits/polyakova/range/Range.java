ad59921a454197c089805f1c2f15dd3638a88a45
Оксана <polyoksi@gmail.com>
2017-09-28 13:13:27 +0700

    Переделала Range до Range*

--- Range/src/ru/academits/polyakova/range/Range.java
+++ Range/src/ru/academits/polyakova/range/Range.java
@@ -3,6 +3,7 @@ package ru.academits.polyakova.range;
 public class Range {
     private double from;
     private double to;
+
     public double getFrom() {
         return from;
     }
@@ -30,6 +31,68 @@ public class Range {
 
     public boolean isInside(double number) {
         return number >= from && number <= to;
+    }
+
+    public Range getCrossing(Range interval1) {
+        double from = interval1.from;
+        double to = interval1.to;
+        if (this.from <= from) {
+            if (this.to >= from) {
+                return new Range(from, Math.min(this.to, to));
+            } else {
+                return null;
+            }
+        } else {
+            if (to >= this.from) {
+                return new Range(this.from, Math.min(this.to, to));
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public Range[] getUnion(Range interval1) {
+        double from = interval1.from;
+        double to = interval1.to;
+        if (this.from <= from) {
+            if (this.to >= from) {
+                return new Range[]{new Range(this.from, Math.max(this.to, to))};
+            } else {
+                return new Range[]{new Range(this.from, this.to), new Range(from, to)};
+            }
+        } else {
+            if (to >= this.from) {
+                return new Range[]{new Range(from, Math.max(this.to, to))};
+            } else {
+                return new Range[]{new Range(from, to), new Range(this.from, this.to)};
+            }
+        }
+    }
+
+    public Range[] getResidual(Range interval1) {
+        double from = interval1.from;
+        double to = interval1.to;
+        if (this.from <= from) {
+            if (this.to > from) {
+                if (this.to != to) {
+                    return new Range[]{new Range(this.from, from), new Range(Math.min(this.to, to), Math.max(this.to, to))};
+                } else {
+                    return new Range[]{new Range(this.from, from)};
+                }
+            } else {
+                return new Range[]{new Range(this.from, this.to)};
+            }
+        } else {
+            if (to > this.from) {
+                if (to != this.to) {
+                    return new Range[]{new Range(from, this.from), new Range(Math.min(this.to, to), Math.max(this.to, to))};
+                } else {
+                    return new Range[]{new Range(from, this.from)};
+                }
+            } else {
+                return new Range[]{new Range(from, to)};
+            }
+        }
     }
 }
 
--- Range/src/ru/academits/polyakova/range/main/Main.java
+++ Range/src/ru/academits/polyakova/range/main/Main.java
@@ -9,12 +9,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число: ");
         double number = scanner.nextDouble();
-        Range range = new Range(4.7, 90.6);
+        Range range = new Range(6,15);
         System.out.printf("Длина отрезка: %.2f%n", range.getLength());
         if (range.isInside(number)) {
-            System.out.println("Ваше число принадлежит отрезку от 4.7 до 90.6");
+            System.out.printf("Ваше число принадлежит отрезку от %.2f до %.2f%n", range.getFrom(), range.getTo());
         } else {
-            System.out.println("Ваше число не принадлежит отрезку от 4.7 до 90.6");
+            System.out.printf("Ваше число не принадлежит отрезку от %.2f до %.2f%n", range.getFrom(), range.getTo());
+        }
+
+        Range interval1 = new Range(1,5);
+        Range resultCrossing = range.getCrossing(interval1);
+        if (resultCrossing != null) {
+            System.out.printf("Пересечение интервалов: [%.2f, %.2f]%n", resultCrossing.getFrom(), resultCrossing.getTo());
+        } else {
+            System.out.println("Интервалы не пересекаются");
+        }
+
+        Range[] resultUnion = range.getUnion(interval1);
+        System.out.print("Объединение интервалов: ");
+        for (Range e : resultUnion) {
+            System.out.printf("[%.2f, %.2f] ", e.getFrom(), e.getTo());
+        }
+
+        Range[] resultResidual = range.getResidual(interval1);
+        System.out.printf("%nРазность интервалов: ");
+        for (Range e : resultResidual) {
+            System.out.printf("[%.2f, %.2f] ", e.getFrom(), e.getTo());
         }
     }
 }
