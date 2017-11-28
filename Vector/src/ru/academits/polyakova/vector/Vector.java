70cf9b415fcd1dac0d3362001fa27b1d43ca818e
Оксана <polyoksi@gmail.com>
2017-11-28 20:25:30 +0700

    Исправила Vector

--- Vector/src/ru/academits/polyakova/vector/Vector.java
+++ Vector/src/ru/academits/polyakova/vector/Vector.java
@@ -1,16 +1,11 @@
 package ru.academits.polyakova.vector;
 
+import javafx.beans.binding.StringBinding;
+
+import java.util.Arrays;
+
 public class Vector {
-
     private double[] vector;
-
-    private Vector(){
-        this.vector = vector;
-    }
-
-    private double[] getVector(){
-        return vector;
-    }
 
     public Vector(int n) {
         if (n <= 0) {
@@ -19,57 +14,45 @@ public class Vector {
         vector = new double[n];
     }
 
-    public Vector(Vector array) {
-        for (int i = 0; i < array.getSize(); i++) {
-           vector[i] = array.getVector()[i];
-        }
+    public Vector(Vector vector) {
+        this.vector = vector.vector;
     }
 
-    public Vector(double[] array) {
-        for (int i = 0; i < array.length; i++) {
-            double elem = array[i];
-            vector[i] = elem;
-        }
+    public Vector(double[] vector) {
+        this.vector = vector;
     }
 
     public Vector(int n, double[] array) {
-        for (int i = 0; i < array.length; i++) {
-            double elem = array[i];
-            vector[i] = elem;
-        }
-        if (array.length < n) {
-            for (int i = array.length; i < vector.length; i++) {
-                vector[i] = 0;
-            }
-        }
+        vector = Arrays.copyOf(array, n);
     }
 
     private int getSize() {
         return vector.length;
-
     }
 
     public String toString() {
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder("{");
         for (int i = 0; i < vector.length; i++) {
-            result.append(vector[i]);
-            if (i != vector.length) {
-                result.append(", ");
+            if (i == vector.length - 1) {
+                result.append(vector[i]);
+                break;
             }
+            result.append(vector[i]);
+            result.append(", ");
         }
-        result.insert(0, "{");
         result.append("}");
         return result.toString();
     }
 
+}
+/*
     public Vector sumVector(Vector vector2) {
-        Vector result = new Vector();
         for (int i = 0; i < this.vector.length; i++) {
             result.getVector()[i] = this.vector[i] + vector2.getVector()[i];
         }
-        return result;
+        return vector;
     }
-
+/*
     public Vector differenceVector(Vector vector2) {
         Vector result = new Vector();
         for (int i = 0; i < this.vector.length; i++) {
@@ -77,4 +60,4 @@ public class Vector {
         }
         return result;
     }
-}
+}*/
--- Vector/src/ru/academits/polyakova/vector/ru/academits/polyakova/vector/main/Main.java
+++ Vector/src/ru/academits/polyakova/vector/ru/academits/polyakova/vector/main/Main.java
@@ -7,10 +7,15 @@ public class Main {
     public static void main(String[] args) {
         double[] array1 = new double[]{1, 2, 3};
         double[] array2 = new double[]{2, 54, 67};
+        double[] array3 = new double[]{2, 5};
         Vector vector1 = new Vector(array1);
         Vector vector2 = new Vector(array2);
+        int n = 3;
+        Vector vector3 = new Vector(4, array3);
         System.out.print(vector1);
-        System.out.println("Сумма векторов: " + vector1.sumVector(vector2));
-        System.out.println("Разность векторов: " + vector1.differenceVector(vector2));
+        System.out.println();
+        System.out.print(vector3);
+        // System.out.println("Сумма векторов: " + vector1.sumVector(vector2));
+        //System.out.println("Разность векторов: " + vector1.differenceVector(vector2));
     }
 }
