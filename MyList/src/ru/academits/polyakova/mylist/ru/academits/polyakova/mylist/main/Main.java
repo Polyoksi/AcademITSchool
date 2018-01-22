11da6d96656e2f52461a30137bfae27f16a3c820
Оксана <polyoksi@gmail.com>
2018-01-22 16:41:48 +0700

    Добавила MyList

---
+++ MyList/src/ru/academits/polyakova/mylist/ru/academits/polyakova/mylist/main/Main.java
@@ -0,0 +1,10 @@
+package ru.academits.polyakova.mylist.ru.academits.polyakova.mylist.main;
+
+import ru.academits.polyakova.mylist.MyList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+
+    }
+}
